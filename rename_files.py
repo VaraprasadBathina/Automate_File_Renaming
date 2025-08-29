f6b3223dc6b6c4328f12d31046cbae02f0587a22
Bannu <vidhyalakshmivaraprasadbathina@gmail.com>
2025-08-29 14:53:04 -0500

    Modified the Code to rename the file

--- rename_files.py
+++ rename_files.py
@@ -2,7 +2,7 @@ import os
 
 folder_path = r'C:\Users\vbath\OneDrive\Desktop\Python Projects\Folder_Rename_Automation\DummyFolder'
 for count, filename in enumerate(os.listdir(folder_path)):
-    dst = f'renamed_{count}{os.path.splitext(filename)}'
+    dst = f'renamed_{count+1}{os.path.splitext(filename)[1]}'
     src = os.path.join(folder_path, filename)
     dst = os.path.join(folder_path, dst)
     os.rename(src, dst)
